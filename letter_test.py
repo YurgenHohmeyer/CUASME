0ba3ab61581aa19abcd1829a59f4fe5b415b2a2e
Yurgen C. Hohmeyer <yurgen@yurgenhohmeyer.com>
2022-03-25 07:34:59 +0000

    Add rotating CU-ASME

--- letter_test.py
+++ letter_test.py
@@ -13,8 +13,8 @@ order = neopixel.GRBW
 numpixels = 64
 
 # Brightness and automatically set colors
-bright = 0.2
-aw = True
+bright = 0.5
+aw = False
 
 # Board setup
 board1 = neopixel.NeoPixel(pin1, numpixels, brightness=bright, auto_write=aw, pixel_order=order)
@@ -22,7 +22,7 @@ board2 = neopixel.NeoPixel(pin2, numpixels, brightness=bright, auto_write=aw, pi
 
 def setpixels(board, locs, color):
     if isinstance(locs, slice):
-        board[locs] = color * len(board[locs])
+        board[locs] = [color] * len(board[locs])
     elif isinstance(locs, list):
         for loc in locs:
             setpixels(board, loc, color)
@@ -31,41 +31,48 @@ def setpixels(board, locs, color):
 
 # For Neopixel Raspberry Pi Code
 cuGold = (255,206,0,0)
-cuGreen = (0,79,66,0)
-backgroundColor = (200,200,200,0)
+cuGreen = (0,220,10,0)
+backgroundColor = (0,0,0,0)
 OFF = (0,0,0,0)
 
-letterColor = cuGreen
+letterColor = cuGold
+i = 1
+while i < 5:
+    # Letter C
+    board1.fill(backgroundColor)
+    setpixels(board1, [10,11,12,13,18,26,34,42,slice(50,54)], letterColor)
 
-# Letter C
-board1.fill(backgroundColor)
-setpixels(board1, [10:14,18,26,34,42,50:54], letterColor)
+    # Letter U
+    board2.fill(backgroundColor)
+    setpixels(board2, [10,13,18,21,26,29,34,37,42,45,50,slice(51,54)], cuGreen)
+    board1.show()
+    board2.show()
+    time.sleep(2)
 
-# Letter U
-board2.fill(backgroundColor)
-setpixels(board2, [10,13,18,21,26,29,34,37,42,45,50,51:54], letterColor)
+    # Letter A
+    board1.fill(backgroundColor)
+    setpixels(board1, [slice(10,14),18,21,slice(26,30),34,37,42,45,50,53], cuGreen)
 
-time.sleep(10)
+    # Letter S
+    board2.fill(backgroundColor)
+    setpixels(board2, [slice(10,14),18,slice(26,29),37,45,slice(50,54)], letterColor)
+    board1.show()
+    board2.show()
+    time.sleep(2)
 
-# Letter A
-board1.fill(backgroundColor)
-setpixels(board1, [10:14,18,21,26:30,34,37,42,45,50,53], letterColor)
+    # Letter M
+    board1.fill(backgroundColor)
+    setpixels(board1, [9,10,13,14,slice(17,23),25,27,28,30,33,38,41,46,49,54], letterColor)
 
-# Letter S
-board2.fill(backgroundColor)
-setpixels(board2, [10:14,18,26:29,37,45,50:54], letterColor)
-
-time.sleep(5)
-
-# Letter M
-board1.fill(backgroundColor)
-setpixels(board1, [9,10,13,14,17:23,25,27,28,30,33,38,41,46,49,54], letterColor)
-
-# Letter E
-board2.fill(backgroundColor)
-setpixels(board2, [10:14,18,26:30,34,42,50:54], letterColor)
-
-time.sleep(5)
+    # Letter E
+    board2.fill(backgroundColor)
+    setpixels(board2, [slice(10,14),18,slice(26,30),34,42,slice(50,54)], cuGreen)
+    board1.show()
+    board2.show()
+    time.sleep(2)
+    i += 1
 
 board1.fill(OFF)
 board2.fill(OFF)
+board1.show()
+board2.show()
